cd5cf222115a5c442392421afad8b1e7aefde4ea
Stefan Zweifel <hello@stefanzweifel.io>
2021-03-26 15:58:14 +0100

    Use Cache System in GlobalTranslator

--- src/Translations/GlobalTranslator.php
+++ src/Translations/GlobalTranslator.php
@@ -3,7 +3,7 @@
 namespace Twomedia\PoliciesBuilder\Translations;
 
 use Illuminate\Cache\CacheManager;
-use Illuminate\Container\Container;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation\FileLoader;
 use Illuminate\Translation\Translator;
@@ -23,42 +23,59 @@ class GlobalTranslator
         'tr',
     ];
 
-    public function __construct()
+    const SECONDS_TO_CACHE_LOCALE_FILES = 60 * 60 * 24;
+
+    /**
+     * @var CacheManager|null
+     */
+    private ?CacheManager $cacheManager;
+
+    public function __construct(CacheManager $cacheManager = null)
     {
+        if ($cacheManager === null) {
+            /** @var CacheManager $cache */
+            $cacheManager = Container::getInstance()->make(CacheManager::class);
+        }
+
+        $this->cacheManager = $cacheManager;
     }
 
-    public static function trans($page, string $key, array $replace = [])
+    public function trans($page, string $key, array $replace = [])
     {
-        $filesystem = new Filesystem();
-
-        $fileLoader = new FileLoader($filesystem, '.');
-
-        $languageToTranslateTo = $page->lang ?? $page->fallbackLocale ?? 'de';
-
-        $translator = new Translator($fileLoader, $languageToTranslateTo);
+        $languageToTranslateTo = $page->lang ?? $page->fallbackLocale;
 
         if (! in_array($languageToTranslateTo, self::SUPPORTED_LANGUAGES)) {
             throw new LogicException("Language {$languageToTranslateTo} not supported");
         }
 
-        /** @var CacheManager $cache */
-        /*$cache = Container::getInstance()->make(CacheManager::class);
+        $remoteLocaleStirngs = $this->cacheManager->remember(
+            "trans::policies::{$languageToTranslateTo}",
+            self::SECONDS_TO_CACHE_LOCALE_FILES,
+            fn () => $this->fetchLocaleStringsForLanguage($languageToTranslateTo)
+        );
 
-        $cache = $cache->store();
+        $translator = $this->setupTranslator($languageToTranslateTo);
 
-        $remoteJson = $cache->remember('remote_json', 1000000000, function () {
-            return json_decode(file_get_contents('http://127.0.0.1:8000/lang/policies.json'), true);
-        });*/
-
-        $remoteJson = json_decode(file_get_contents("http://127.0.0.1:8000/lang/{$languageToTranslateTo}/policies.json"), true);
-
-        $translator->addLines($remoteJson, $languageToTranslateTo);
-
-        /*foreach (self::SUPPORTED_LANGUAGES as $language) {
-            $remoteJson = json_decode(file_get_contents("http://127.0.0.1:8000/lang/{$language}/policies.json"), true);
-            $translator->addLines($remoteJson, $language);
-        }*/
+        $translator->addLines($remoteLocaleStirngs, $languageToTranslateTo);
 
         return $translator->get($key, $replace);
+    }
+
+    protected function fetchLocaleStringsForLanguage(string $language): array
+    {
+        // return json_decode(file_get_contents("https://v2.webservice.apy.ch/lang/{$language}/policies.json"), true);
+        return json_decode(file_get_contents("http://127.0.0.1:8000/lang/{$language}/policies.json"), true);
+    }
+
+    /**
+     * @param string $languageToTranslateTo
+     * @return Translator
+     */
+    protected function setupTranslator(string $languageToTranslateTo): Translator
+    {
+        $filesystem = new Filesystem();
+        $fileLoader = new FileLoader($filesystem, '.');
+
+        return new Translator($fileLoader, $languageToTranslateTo);
     }
 }
