75f2c96a85808a668f70b6247af64a5ae33b018f
Stefan Zweifel <hello@stefanzweifel.io>
2021-04-30 10:02:55 +0200

    Add ImprintTest

---
+++ tests/Policies/ImprintTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Twomedia\PoliciesBuilder\Tests\Policies;
+
+use Stringable;
+use Twomedia\PoliciesBuilder\DTOs\Copyright;
+use Twomedia\PoliciesBuilder\DTOs\IconCopyright;
+use Twomedia\PoliciesBuilder\Policies\Imprint;
+use PHPUnit\Framework\TestCase;
+
+class ImprintTest extends TestCase
+{
+    /** @test */
+    public function casts_image_copyrights_to_strings()
+    {
+        $imprint = Imprint::make()
+            ->imageCopyrights([
+                Copyright::make('Picasso', 'example.com'),
+                IconCopyright::make('thenounproject.com'),
+                new class() implements Stringable {
+                    public function __toString()
+                    {
+                        return 'Anonymous Copyright Class';
+                    }
+                },
+            ]);
+
+        $this->assertEquals([
+            'imageCopyrights' => [
+                '© Picasso / example.com',
+                'Icons © thenounproject.com',
+                'Anonymous Copyright Class',
+            ],
+        ], $imprint->placeholders());
+    }
+}
