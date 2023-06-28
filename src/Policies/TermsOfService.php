7565ae63839a91a01187e7969000a1f60a604bcf
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-06-28 11:56:53 +0200

    Add onBehalfOf

--- src/Policies/TermsOfService.php
+++ src/Policies/TermsOfService.php
@@ -19,14 +19,23 @@ class TermsOfService implements Policy, CanBeBuiltInJigsaw
 
     public function placeholders(): array
     {
-        return [];
+        return $this->placeholders;
     }
 
-    public function inCooperationWith(CooperationPartner $partner)
+    public function inCooperationWith(CooperationPartner $partner): self
     {
         $this->placeholders['cooperation_partner_legal_name'] = $partner->legalName;
         $this->placeholders['cooperation_partner_name'] = $partner->name;
         $this->placeholders['cooperation_partner_url'] = $partner->url;
+
+        return $this;
+    }
+
+    public function onBehalfOf(CooperationPartner $partner): self
+    {
+        $this->placeholders['behalf_of_legal_name'] = $partner->legalName;
+        $this->placeholders['behalf_of_name'] = $partner->name;
+        $this->placeholders['behalf_of_url'] = $partner->url;
 
         return $this;
     }
