a1f8ad9547bac9b09059fd659641321e3b9ffdbf
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-06-28 11:11:16 +0200

    Add inCooperationWith method

--- src/Policies/TermsOfService.php
+++ src/Policies/TermsOfService.php
@@ -4,6 +4,7 @@ namespace Twomedia\PoliciesBuilder\Policies;
 
 use Twomedia\PoliciesBuilder\Contracts\CanBeBuiltInJigsaw;
 use Twomedia\PoliciesBuilder\Contracts\Policy;
+use Twomedia\PoliciesBuilder\DTOs\CooperationPartner;
 
 class TermsOfService implements Policy, CanBeBuiltInJigsaw
 {
@@ -19,6 +20,15 @@ class TermsOfService implements Policy, CanBeBuiltInJigsaw
     public function placeholders(): array
     {
         return [];
+    }
+
+    public function inCooperationWith(CooperationPartner $partner)
+    {
+        $this->placeholders['cooperation_partner_legal_name'] = $partner->legalName;
+        $this->placeholders['cooperation_partner_name'] = $partner->name;
+        $this->placeholders['cooperation_partner_url'] = $partner->url;
+
+        return $this;
     }
 
     public function jigsawPathName(): string
